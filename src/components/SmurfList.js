5521ffb1ee34bc4b863fda2a5db254a77675cb84
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-03-19 07:54:30 -0700

    SmurfList connect - passing props to Smurf

--- src/components/SmurfList.js
+++ src/components/SmurfList.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import { connect } from "react-redux";
 import Smurf from './Smurf';
 
- const SmurfList = ()=> {
-    const isLoading = false;
-    const testSmurf = {
-        id:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
-        name:'Poppa Smurf',
-        position:'Village Leader',
-        nickname: 'Pops',
-        description: 'Papa is the practical village leader and the father figure of 100 or so young Smurfs. He is easily identified by his red Smurf hat, pants, and a shortly-trimmed white beard and moustache.'
-    }
+ const SmurfList = (props) => {
 
-    if (isLoading) {
+    if (props.isLoading) {
         return <h1>Loading...</h1>;
     }
 
-    return(<div className="listContainer">
-        <Smurf smurf={testSmurf}/>
-    </div>);
+    return(
+    <>
+        {props.smurfsData && props.smurfsData.map(smurf => (
+            <Smurf key={smurf.id} smurf={smurf} />
+        ))}
+    </>
+    );
 }
 
-export default SmurfList;
+const mapStateToProps = state => {
+    return {
+        smurfsData: state.smurfsData,
+        isLoading: state.isLoading
+    }
+}
+
+export default connect(mapStateToProps)(SmurfList);
 
 //Task List:
 //1. Connect the smurfs and loading state values to the SmurfList component.
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,36 @@
+import { FETCH_SMURFS_START, FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FAIL } from "../actions";
+
 
 export const initialState = {
+    smurfsData: [],
+    isLoading: false,
+    error: ''
 }
 
-const reducer = ()=>{
+const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        case FETCH_SMURFS_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: ''
+            }
+        case FETCH_SMURFS_SUCCESS:
+            return {
+                ...state,
+                smurfsData: action.payload,
+                isLoading: false
+            }
+        case FETCH_SMURFS_FAIL:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+
 }
 
 export default reducer;
